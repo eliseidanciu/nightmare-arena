ef5a35b6de091c77a8668e6b3c856263bd4f220f
Elisei Danciu <eliseidanciu@gmail.com>
2018-03-18 17:14:19 +0100

    Added player and camera controllers

---
+++ root/Assets/Characters/FantasyWizard/Script/Wizard.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : MonoBehaviour {
+
+	public const string IDLE	= "Wizard_Idle";
+	public const string RUN		= "Wizard_Run";
+	public const string ATTACK	= "Wizard_Attack";
+	public const string SKILL	= "Wizard_Skill";
+	public const string DAMAGE	= "Wizard_Damage";
+	public const string STUN	= "Wizard_Stun";
+	public const string DEATH	= "Wizard_Death";
+
+	Animation anim;
+
+	void Start () {
+		anim = GetComponent<Animation>();
+	}
+
+	public void IdleAni (){
+		anim.CrossFade (IDLE);
+	}
+
+	public void RunAni (){
+		anim.CrossFade (RUN);
+	}
+
+	public void AttackAni (){
+		anim.CrossFade (ATTACK);
+	}
+
+	public void SkillAni (){
+		anim.CrossFade (SKILL);
+	}
+
+	public void DamageAni (){
+		anim.CrossFade (DAMAGE);
+	}
+
+	public void StunAni (){
+		anim.CrossFade (STUN);
+	}
+
+	public void DeathAni (){
+		anim.CrossFade (DEATH);
+	}
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ root/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    public float x,y,z;
+    float smoothSpeed = 0.1f;
+    Vector3 offset;
+
+	void Start () {
+        offset = new Vector3(x, y, z);
+    }
+	
+	void LateUpdate () {
+        Vector3 desiredPosition = player.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
