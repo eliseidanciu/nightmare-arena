49d056e92396c5578159a5354503363ccfcab1ce
Elisei Danciu <eliseidanciu@gmail.com>
2018-03-25 19:27:59 +0200

    Added Character.cs class

---
+++ root/Assets/Scripts/Character.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour, IDamageable
+{
+    public float hp;
+    public float armor;
+    public float attackPower;
+    public float attackSpeed; //hits per minute
+    public float moveSpeed;
+    public bool isAlive;
+
+    protected float msBetweenAttacks;
+    protected float nextAttackTime;
+
+    protected Animator animator;
+    protected GameObject deathParticles;
+    protected Transform target;
+
+    public Character()
+    {
+        hp = 100f;
+        armor = 0f;
+        attackPower = 10f;
+        attackSpeed = 60f;
+        moveSpeed = 10f;
+        isAlive = true;
+        msBetweenAttacks = 60 / attackSpeed * 1000;
+    }
+
+    public Character(float _hp, float _armor, float _attackPower, float _attackSpeed, float _moveSpeed, bool _isAlive)
+    {
+        PropertyValidation(0f, 100f, out hp, _hp);
+        PropertyValidation(0f, 100f, out armor, _armor);
+        PropertyValidation(0f, 100f, out attackPower, _attackPower);
+        PropertyValidation(30f, 100f, out attackSpeed, _attackSpeed);
+        PropertyValidation(3f, 20f, out moveSpeed, _moveSpeed);
+        isAlive = _isAlive;
+        msBetweenAttacks = (60 / attackSpeed) * 1000;
+    }
+
+    public void TakeDamage(float enemyAttackPower)
+    {
+        animator.Play("TakeDamage");
+        float damage = enemyAttackPower / 100 * armor;
+        hp = hp - damage;
+        if(hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        animator.Play("Die");
+        isAlive = false;
+            Destroy(gameObject);
+            Instantiate(deathParticles, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(deathParticles, 1f);
+        
+    }
+
+    public void CloseRangedAttack(/*IDamageable enemy*/)
+    {
+        if (Time.time > nextAttackTime)
+        {
+            nextAttackTime = Time.time + (msBetweenAttacks / 1000);
+            animator.SetTrigger("CloseRangedAttack");
+            Debug.Log("Attack Animation");
+            
+            // enemy.TakeDamage(attackPower);
+            
+        }
+    }
+
+    public abstract void Move();
+
+    /*
+    public void LongRangedAttack(Character enemy)
+    {
+        if (Time.time > nextAttackTime)
+        {
+            animation.Play("LongRangedAttack");
+            if (animation.IsPlaying("LongRangedAttack"))
+            {
+                Projectile newProjectile = Instantiate(Projectile, )
+            }
+        }
+
+    }*/
+
+    private void PropertyValidation(float minValue, float maxValue, out float value, float desiredValue)
+    {
+        if (desiredValue > maxValue)
+        {
+            value = maxValue;
+        }
+        else if (desiredValue < minValue)
+        {
+            value = minValue;
+        }
+        else
+        {
+            value = desiredValue;
+        }
+    }
+}
--- root/Assets/Scripts/Player.cs
+++ root/Assets/Scripts/Player.cs
@@ -2,11 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(PlayerController))]
-public class Player : MonoBehaviour
+public class Player : Character
 {
-
-    float moveSpeed = 10f;
-
     Camera viewCamera;
     PlayerController controller;
 
@@ -14,16 +11,22 @@ public class Player : MonoBehaviour
     {
         controller = GetComponent<PlayerController>();
         viewCamera = Camera.main;
+        animator = GetComponent<Animator>();
     }
-
-
-
+    
     void Update()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
-        controller.Move(moveVelocity);
+        Move();
+        CameraFollow();
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CloseRangedAttack();
+        }
+      
+    }
 
+    public void CameraFollow()
+    {
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayDistance;
@@ -35,5 +38,13 @@ public class Player : MonoBehaviour
             //Debug.DrawRay(ray.origin,ray.direction * 100,Color.red);
             controller.LookAt(point);
         }
+    }
+
+    public override void Move()
+    {
+        animator.SetTrigger("Move");
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
     }
 }
