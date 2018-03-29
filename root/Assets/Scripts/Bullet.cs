d171d01162288ba29a2369a8136df377a937790f
Elisei Danciu <eliseidanciu@gmail.com>
2018-03-29 16:50:49 +0200

    Fixed bullet not detecting collision with terrain

--- root/Assets/Scripts/Bullet.cs
+++ root/Assets/Scripts/Bullet.cs
@@ -6,27 +6,28 @@ public class Bullet : MonoBehaviour {
     float speed;
     float lifeTime;
     public Character attacker;
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         speed = 50;
         lifeTime = 3f;
         Destroy(gameObject, lifeTime);
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-	}
+        rb.velocity = transform.forward * speed;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        Destroy(gameObject);
         if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Player"))
         {
             var enemy = other.gameObject.GetComponent<Character>();
             enemy.TakeDamage(attacker.attackPower);
-            Destroy(gameObject);
-            Debug.Log(enemy.hp);
         }
     }
 }
