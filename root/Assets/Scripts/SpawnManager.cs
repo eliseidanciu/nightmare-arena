d4a63aeee3350ac32c058e13a57ca4be34132f26
Elisei Danciu <eliseidanciu@gmail.com>
2018-05-13 17:27:08 +0200

    Added spawn and difficulty system

---
+++ root/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    public Transform[] spawnPoints;
+    public Enemy[] enemies;
+
+    float spawnDelay;
+    float timeSinceLastSpawn;
+
+    float timeBetweenDifficultyIncrements;
+    float timeSinceLastDifficultyIncrement;
+    float difficultyMultiplier;
+
+    void Start()
+    {
+        spawnDelay = 5.0f;
+        difficultyMultiplier = 1.2f;
+        timeSinceLastSpawn = 0.0f;
+        timeBetweenDifficultyIncrements = 10.0f;
+        timeSinceLastDifficultyIncrement = 0.0f;
+    }
+    
+
+    void Update()
+    {
+        SpawnMonsters();
+        IncreaseDifficulty();
+    }
+
+    void IncreaseDifficulty()
+    {
+        timeSinceLastDifficultyIncrement += Time.deltaTime;
+        if (timeSinceLastDifficultyIncrement >= timeBetweenDifficultyIncrements && spawnDelay >= 1.0f)
+        {
+            spawnDelay /= difficultyMultiplier;
+            timeSinceLastDifficultyIncrement = 0.0f;
+            Debug.Log("Difficulty increased. Spawn delay: " + spawnDelay);
+        }
+    }
+
+    void SpawnMonsters()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (spawnDelay <= timeSinceLastSpawn)
+        {
+            timeSinceLastSpawn = 0.0f;
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                Instantiate(enemies[i], spawnPoints[i]);
+            }
+        }
+    }
+    
+
+}
