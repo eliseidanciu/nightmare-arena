f7ed5df1a5cfae04f56d61736809ed8eb3e7e98b
Elisei Danciu <eliseidanciu@gmail.com>
2018-06-08 09:42:33 +0200

    Added highscore system

--- root/Assets/Scripts/ScoreManager.cs
+++ root/Assets/Scripts/ScoreManager.cs
@@ -1,23 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
-    public Text scoreText;
     public static int score;
-    public static int highScore;
-    
+    int highScore;
 
-	void Start () {
-        score = 0;
+    GameObject scoreObj;
+    GameObject highScoreObj;
+    Text scoreText;
+    Text highScoreText;
+
+    const int GAME_OVER_INDEX = 2;
+    const int GAMEPLAY_INDEX = 1;
+
+    void Start () {
+
+        scoreObj = GameObject.FindGameObjectWithTag("Score");
+        scoreText = scoreObj.GetComponent<Text>();
         scoreText.text = score.ToString();
-	}
+
+        if (SceneManager.GetActiveScene().buildIndex == GAME_OVER_INDEX)
+        {
+            scoreText.text = "Score: " + score.ToString();
+            Debug.Log("Score: " + score.ToString());
+            highScoreObj = GameObject.FindGameObjectWithTag("HighScore");
+            highScoreText = highScoreObj.GetComponent<Text>();
+            CheckHighScore();
+        }
+        else
+        {
+            score = 0;
+        }
+    }
 	
 	void Update () {
-        scoreText.text = score.ToString();
         
+        if(SceneManager.GetActiveScene().buildIndex == GAMEPLAY_INDEX)
+        {
+            scoreText.text = score.ToString();
+        }
+            
+    }
+
+    void CheckHighScore()
+    {
+        const string PATH = "..\\root\\Assets\\Saves\\highScore.txt";
+        int oldHighScore = 0;
+
+        if (File.Exists(PATH))
+        {
+            Debug.Log("File exists");
+            string oldHighScoreString = File.ReadAllText(PATH);
+            Int32.TryParse(oldHighScoreString, out oldHighScore);
+            Debug.Log("Old HighScore: " + oldHighScore);
+        }
+        if (score > oldHighScore)
+        {
+            highScore = score;
+            File.WriteAllText(PATH, highScore.ToString());
+            Debug.Log("New High Score " + highScore);
+        }
+        else
+        {
+            highScore = oldHighScore;
+        }
+        highScoreText.text = "High Score: " + highScore.ToString();
     }
 
 
