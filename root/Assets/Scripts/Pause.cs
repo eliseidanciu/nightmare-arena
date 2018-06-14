10093b83c798eff68a7f4c1a4dad1feea56111d9
Scrivere <leoscrivere@hotmail.it>
2018-06-14 18:49:45 +0200

    Commit Bello

---
+++ root/Assets/Scripts/Pause.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public bool gameIsPaused = false;
+    public GameObject pauseMenu;
+    private object otherObject;
+    AudioSource audioSource;
+
+
+    void Start () {
+        // audioSource = GameObject.Find("Player").GetComponent<AudioSource>();
+        otherObject = GameObject.Find("CameraHolder").GetComponent<CameraController>(); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if(gameIsPaused)
+            {
+                otherObject = GameObject.Find("CameraHolder").GetComponent<CameraController>().enabled = true;
+                AudioListener.volume = 1f;
+                Resume();
+               
+            }
+        else
+        {
+                otherObject = GameObject.Find("CameraHolder").GetComponent<CameraController>().enabled=false;
+                AudioListener.volume = 0f;
+                //audioSource.mute=true;
+                MenuStart();
+
+            }
+        }
+
+    }
+
+
+     public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+     public void MenuStart()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+    
+        
+    }
+
+
---
+++ root/Assets/Scripts/Roba.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roba : MonoBehaviour
+{
+    public GameObject Prefab;
+
+    public Transform spawnPoint2;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Test();
+    }
+
+    public void Test()
+    {
+        var mostro = Instantiate(Prefab);
+
+        //PROGRAMMING 101
+        mostro.transform.position = spawnPoint2.transform.position;
+    }
+}
